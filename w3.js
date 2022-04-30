37d9b9c522af6080f4e53905c6dc6c30e67089b4
hussain <abdulbaqhussain2018@gmail.com>
2022-04-30 07:08:00 -0700

    my first comit

---
+++ w3.js
@@ -0,0 +1,17 @@
+var myMenu = document.getElementById("menu");
+myMenu.addEventListener("click", function(){
+    document.getElementById("drop-down").classList.toggle("active")
+});
+var myMenu1 = document.getElementById("menu1");
+myMenu1.addEventListener("click", function(){
+    document.getElementById("drop-down1").classList.toggle("active")
+});
+var myMenu2 = document.getElementById("menu2");
+myMenu2.addEventListener("click", function(){
+    document.getElementById("drop-down2").classList.toggle("active")
+});
+// Comment
+
+
+
+
